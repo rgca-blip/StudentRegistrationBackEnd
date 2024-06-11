dec3cffd939074b56ac9e92558cb83d24ba2ddea
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 <rgsoftware1@gmail.com>
2024-06-11 15:52:07 +0530

    main class modified

--- src/main/java/com/rgsoftware/StudentRegistrationBackEnd/StudentRegistrationBackEndApplication.java
+++ src/main/java/com/rgsoftware/StudentRegistrationBackEnd/StudentRegistrationBackEndApplication.java
@@ -9,7 +9,7 @@ public class StudentRegistrationBackEndApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(StudentRegistrationBackEndApplication.class, args);
 		System.out.println("HI");
-		System.out.println("bye");
+		//System.out.println("bye");
 	}
 
 }
